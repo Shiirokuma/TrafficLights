ca014f2ae75db828d44b57b99296e02e090ad4f4
Shiirokuma <florian.lrntz@eduge.ch>
2019-01-21 16:00:48 +0100

    Création test TDD

---
+++ TrafficLightsGroupe4/UnitTestTrafficLights/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTrafficLights")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestTrafficLights")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("23dedef4-1534-4e7c-9ce4-9e08a6974d2e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TrafficLightsGroupe4/UnitTestTrafficLights/TDDTrafficLights.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTrafficLights
+{
+    [TestClass]
+    public class TDDTrafficLights
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
