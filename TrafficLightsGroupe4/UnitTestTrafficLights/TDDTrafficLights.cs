53715b27fc76b94662f0dbbc442ad7a10d2daa28
Shiirokuma <florian.lrntz@eduge.ch>
2019-01-24 15:15:36 +0100

    Ajouter des tests freiner et accelerer dans le TDD

--- TrafficLightsGroupe4/UnitTestTrafficLights/TDDTrafficLights.cs
+++ TrafficLightsGroupe4/UnitTestTrafficLights/TDDTrafficLights.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TrafficLightsGroupe4;
 
@@ -7,10 +8,46 @@ namespace UnitTestTrafficLights
     [TestClass]
     public class TDDTrafficLights
     {
-        [TestMethod]
-        public void Bobo()
+        List<Vehicules> vehicules;
+
+        [TestInitialize]
+        public void InitialisationListVehicules()
         {
-            Velo velo = new Velo();
+            vehicules = new List<Vehicules> {
+            new Velo(),
+            new Voiture(),
+            new Tram(),
+            new Moto(),
+            new Camion()
+            };
+        }
+
+        [TestMethod]
+        public void CheckAccelerer()
+        {
+            foreach (var item in vehicules)
+            {
+                double oldSpeed = item.Vitesse;
+                item.Accelerer();
+                Assert.AreNotEqual(oldSpeed, item.Vitesse);
+            }
+        }
+
+        [TestMethod]
+        public void CheckFreiner()
+        {
+            foreach (var item in vehicules)
+            {
+                double oldSpeed = item.Vitesse;
+                item.Freiner();
+                Assert.AreNotEqual(oldSpeed, item.Vitesse);
+            }
+        }
+
+        [TestCleanup]
+        public void CleanListVehicules()
+        {
+            vehicules = null;
         }
     }
 }
