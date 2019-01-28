cbacb5ea999f0445be45e10868970af32d9bff16
MARTIG JOEY <joey.mrtg@eduge.ch>
2019-01-28 15:31:44 +0100

    Ajout d'un tableau pour l'utilisation des images

--- TrafficLightsGroupe4/TrafficLightsGroupe4/TrafficLights.cs
+++ TrafficLightsGroupe4/TrafficLightsGroupe4/TrafficLights.cs
@@ -20,6 +20,19 @@ namespace TrafficLightsGroupe4
 {
     public partial class TrafficLights : Form
     {
+        /*
+         * Liste des images (png)
+         * 0 Bus                   10 FeuVertBottom             20 Null
+         * 1 CederPassageDown      11 FeuVertLeft               21 velo
+         * 2 CederPassageUp        12 FeuVertRight              22 voitureBOTTOM
+         * 3 CedezLePassage        13 FeuVertTop                23 voitureLEFT
+         * 4 FeuRouge              14 feuxPietons               24 voitureRIGHT
+         * 5 FeuRougeBottom        15 FeuxPietonsRougeLeft      25 voitureTOP
+         * 6 FeuRougeLeft          16 FeuxPietonsRougeRight
+         * 7 FeuRougeRight         17 FeuxPietonsVertLeft
+         * 8 FeuRougeTop           18 FeuxPietonsVertRight
+         * 9 FeuVert               19 motoTOP
+         */
         public TrafficLights()
         {
             InitializeComponent();
