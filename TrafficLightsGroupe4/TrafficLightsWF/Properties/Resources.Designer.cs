0490f39bda29f31bfeed5ccb9f5303fa0b0b7fa0
MARTIG JOEY <joey.mrtg@eduge.ch>
2019-01-31 13:30:01 +0100

    Ajout des routes

--- TrafficLightsGroupe4/TrafficLightsWF/Properties/Resources.Designer.cs
+++ TrafficLightsGroupe4/TrafficLightsWF/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace TrafficLightsWF.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap route1 {
+            get {
+                object obj = ResourceManager.GetObject("route1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
